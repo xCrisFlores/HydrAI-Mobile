6db65322c719ec7504120fa42b7434fd233e8230
xCrisFlores <cristianxd235@gmail.com>
2025-11-08 00:22:49 -0600

    Commit inicial

--- urls/urls.ts
+++ urls/urls.ts
@@ -1,12 +1,13 @@
-/*
-export const WEBSOCKET_URL_LOCAL = "ws://192.168.100.88:8000/ws"
-export const API_URL_LOCAL = "http://192.168.100.88:8001/api"
-export const HYDRAI_NET_URL_LOCAL = "http://192.168.100.88:8002"
-export const LSTM_URL_LOCAL = "http://192.168.100.88:8003"
-*/
 
-//urls movil
+export const WEBSOCKET_URL_LOCAL = "ws://192.168.100.89:8000/ws"
+export const API_URL_LOCAL = "http://192.168.100.89:8001/api"
+export const HYDRAI_NET_URL_LOCAL = "http://192.168.100.89:8002"
+export const LSTM_URL_LOCAL = "http://192.168.100.89:8003"
+
+
+/*
 export const WEBSOCKET_URL_LOCAL = "ws://10.120.65.242:8000/ws"
 export const API_URL_LOCAL = "http://10.120.65.242:8001/api"
 export const HYDRAI_NET_URL_LOCAL = "http://10.120.65.242:8002"
 export const LSTM_URL_LOCAL = "http://10.120.65.242:8003"
+*/
